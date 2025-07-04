c460eb43a2cd808ed33a673f1d2680f4b4778807
Nour Shreif <nourshraif4@gmail.com>
2025-07-04 22:48:04 +0300

    Ex3Med

---
+++ Ex2Med.py
@@ -0,0 +1,13 @@
+def countWords(text):
+    dict={}
+    cleanText= text.lower().replace(".", "")
+    for word in cleanText.lower().split():
+        dict[word]=0
+    for word in cleanTextG.lower().split():
+        dict[word]+=1
+    return dict       
+
+
+
+message="AI is the future. The future is now"
+print(countWords(message))
