c4a40a166fa92ddd003c019d38230447b7da6df8
Nour Shreif <nourshraif4@gmail.com>
2025-07-04 18:09:51 +0300

    Ex3Sol

---
+++ Ex3.py
@@ -0,0 +1,14 @@
+def sliceFirstLast(list):
+    newlist=[]
+    newlist.append(list[0])
+    newlist.append(list[-1])
+    return newlist
+    
+
+
+
+
+listA=["nourrr",2,3,4,]
+newList=sliceFirstLast(listA)
+print(newList)
+
